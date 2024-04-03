84329f4d0605bd479c5de7f066f96641c3da7a8b
KevinKing66 <kevinvallenato2002@gmail.com>
2024-04-03 15:24:02 -0500

    start bot

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 import telebot
 
+
+# load environment variables from file ".env"
+load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-print(BOT_TOKEN)
+
 bot = telebot.TeleBot(BOT_TOKEN)
-"""
+# Received msg as /start or /hello to 
 @ bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
@@ -13,4 +17,5 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text) 
-"""
+
+bot.infinity_polling()
